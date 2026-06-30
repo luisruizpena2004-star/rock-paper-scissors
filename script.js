494a37fded4a0aa73251a37a407dc0beebbfcd9e
Luis Ruiz Peña <luisruizpena2004@gmail.com>
2026-06-30 19:10:40 -0400

    Implement function for getting computer choice

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-"use strict";
-console.log("hello world");
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function getComputerChoice() {
+  let choice = "";
+  let randomNumber = getRandomInt(3);
+  switch (randomNumber) {
+    case 0:
+      choice = "Rock";
+      break;
+    case 1:
+      choice = "Paper";
+      break;
+    case 2:
+      choice = "Scissors";
+      break;
+  }
+  return choice;
+}
