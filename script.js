2b377aefb091afe5bcf8d3bb4764dd1f6554c314
Luis Ruiz Peña <luisruizpena2004@gmail.com>
2026-06-30 19:20:10 -0400

    Implment function for getting player's choice

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
+function capitalizeFirstLetter(val) {
+  return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+}
+
 function getComputerChoice() {
   let choice = "";
   let randomNumber = getRandomInt(3);
@@ -18,3 +22,10 @@ function getComputerChoice() {
   }
   return choice;
 }
+
+function getHumanChoice() {
+  let choice = prompt("Rock, Paper or Scissors? ").trim().toLowerCase();
+  return capitalizeFirstLetter(choice);
+}
+
+console.log(getHumanChoice());
