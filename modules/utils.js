2edc214927b2bfe35cd6b39972194e6c77fa4091
Luis Ruiz Peña <luisruizpena2004@gmail.com>
2026-06-30 23:48:28 -0400

    Refactor implementation

---
+++ modules/utils.js
@@ -0,0 +1,14 @@
+export function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+export function getChoiceFromInt(val) {
+  switch (val) {
+    case 1:
+      return "Rock";
+    case 2:
+      return "Paper";
+    case 3:
+      return "Scissors";
+  }
+}
--- script.js
+++ script.js
@@ -1,91 +1,71 @@
+import { getRandomInt, getChoiceFromInt } from "./modules/utils.js";
+
 playGame();
 
-// Game functions
 function getComputerChoice() {
-  let choice = "";
-  let randomNumber = getRandomInt(3);
-  switch (randomNumber) {
-    case 0:
-      choice = "Rock";
-      break;
-    case 1:
-      choice = "Paper";
-      break;
-    case 2:
-      choice = "Scissors";
-      break;
-  }
-  return choice;
+  let randomNumber = getRandomInt(3) + 1;
+  return randomNumber;
 }
 
 function getHumanChoice() {
   let choice = prompt("Rock, Paper or Scissors? ").trim().toLowerCase();
-  return capitalizeFirstLetter(choice);
+  switch (choice) {
+    case "rock":
+      return 1;
+    case "paper":
+      return 2;
+    case "scissors":
+      return 3;
+  }
 }
 
 function playRound(humanChoice, computerChoice) {
-  let winner;
-  //Draw
   if (humanChoice === computerChoice) {
-    winner = null;
-    //Human Win situations
-  } else if (humanChoice === "Paper" && computerChoice === "Rock") {
-    winner = "Human";
-  } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
-    winner = "Human";
-  } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
-    winner = "Human";
-    //Computer Win situations
-  } else if (computerChoice === "Paper" && humanChoice === "Rock") {
-    winner = "Computer";
-  } else if (computerChoice === "Rock" && humanChoice === "Scissors") {
-    winner = "Computer";
-  } else if (computerChoice === "Scissors" && humanChoice === "Paper") {
-    winner = "Computer";
+    console.log(`You draw! both chose ${getChoiceFromInt(humanChoice)}`);
+    return "draw";
+  } else if (
+    humanChoice - computerChoice === 1 ||
+    humanChoice - computerChoice === -2
+  ) {
+    return "human";
+  } else {
+    return "computer";
   }
-  console.log(`Player: ${humanChoice}`);
-  console.log(`Computer: ${computerChoice}`);
-  return winner;
 }
 
 function playGame() {
-  let roundWinner;
+  let winner;
+  let humanChoice;
+  let computerChoice;
+
   let humanScore = 0;
   let computerScore = 0;
-  let humanSelection;
-  let computerSelection;
+
   for (let i = 1; i <= 5; i++) {
-    console.log(`ROUND ${i}`);
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    roundWinner = playRound(humanSelection, computerSelection);
-    if (roundWinner === "Human") {
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    winner = playRound(humanChoice, computerChoice);
+    if (winner === "human") {
       humanScore++;
-      console.log(`You win! ${humanSelection} beats ${computerSelection}`);
-    } else if (roundWinner === "Computer") {
+      console.log(
+        `You win the round! ${getChoiceFromInt(humanChoice)} beats ${getChoiceFromInt(computerChoice)}`,
+      );
+    } else if (winner === "computer") {
       computerScore++;
-      console.log(`You lose! ${computerSelection} beats ${humanSelection}`);
+      console.log(
+        `You lose the round! ${getChoiceFromInt(computerChoice)} beats ${getChoiceFromInt(humanChoice)}`,
+      );
     } else {
-      console.log(`Draw! Both chose ${humanSelection}`);
+      console.log(`Draw! Both chose ${getChoiceFromInt(computerChoice)}`);
     }
   }
-  console.log("RESULTS");
+
   if (humanScore === computerScore) {
-    console.log("Draw!");
+    console.log(`Draw!`);
   } else if (humanScore > computerScore) {
-    console.log("You Win!");
+    console.log(`You Win!`);
   } else {
-    console.log("You Lose!");
+    console.log(`You Lose!`);
   }
-  console.log(`Player Score: ${humanScore}`);
-  console.log(`Computer Score: ${computerScore}`);
-}
-
-// Utils
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
-function capitalizeFirstLetter(val) {
-  return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+  console.log(`Human Score: ${humanScore}, Computer Score: ${computerScore}`);
 }
