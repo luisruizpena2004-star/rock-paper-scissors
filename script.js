047497c625fe9bfe68dc11e66d362c86533931c0
Luis Ruiz Peña <luisruizpena2004@gmail.com>
2026-06-30 20:28:49 -0400

    Implement a game round

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let humanScore = 0;
 let computerScore = 0;
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
 // Game functions
 function getComputerChoice() {
   let choice = "";
@@ -22,6 +27,40 @@ function getComputerChoice() {
 function getHumanChoice() {
   let choice = prompt("Rock, Paper or Scissors? ").trim().toLowerCase();
   return capitalizeFirstLetter(choice);
+}
+
+function playRound(humanChoice, computerChoice) {
+  let winner;
+  //Draw
+  if (humanChoice === computerChoice) {
+    winner = null;
+    //Human Win situations
+  } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+    winner = "Human";
+  } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+    winner = "Human";
+  } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+    winner = "Human";
+    //Computer Win situations
+  } else if (computerChoice === "Paper" && humanChoice === "Rock") {
+    winner = "Computer";
+  } else if (computerChoice === "Rock" && humanChoice === "Scissors") {
+    winner = "Computer";
+  } else if (computerChoice === "Scissors" && humanChoice === "Paper") {
+    winner = "Computer";
+  }
+  console.log(`Player: ${humanChoice}`);
+  console.log(`Computer: ${computerChoice}`);
+  if (winner === "Human") {
+    humanScore++;
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+  } else if (winner === "Computer") {
+    computerScore++;
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+  } else {
+    console.log(`Draw! Both chose ${humanChoice}`);
+  }
+  return;
 }
 
 // Utils
