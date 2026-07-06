5d67931cb7a1f148a78aacf37c9e5be26e25e005
Luis Ruiz Peña <luisruizpena2004@gmail.com>
2026-07-06 13:01:56 -0400

    Refactor for playing rounds using the UI

--- script.js
+++ script.js
@@ -1,68 +1,54 @@
 import { getRandomInt, getChoiceFromInt } from "./modules/utils.js";
 
+const choiceBtns = document.querySelector("#choiceBtns");
+const resultDiv = document.querySelector("#result");
+const humanScoreLabel = document.querySelector("#humanScore");
+const computerScoreLabel = document.querySelector("#computerScore");
+
 let humanScore = 0;
 let computerScore = 0;
 
-playGame();
+choiceBtns.addEventListener("click", (e) => {
+    // Getting the choices as an int(1,2 or 3) makes the result calculation easier
+    let humanChoice = Number(e.target.dataset.value);
+    let computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+});
 
 function getComputerChoice() {
     let randomNumber = getRandomInt(3) + 1;
     return randomNumber;
 }
 
-function getHumanChoice() {
-    let choice = prompt("Rock, Paper or Scissors? ").trim().toLowerCase();
-    // Returning the value as a number allow us to use a simple formula to get the winner
-    switch (choice) {
-        case "rock":
-            return 1;
-        case "paper":
-            return 2;
-        case "scissors":
-            return 3;
-    }
-}
-
 function playRound(humanChoice, computerChoice) {
+    let labelHumanChoice = getChoiceFromInt(humanChoice);
+    let labelComputerChoice = getChoiceFromInt(computerChoice);
+
     if (humanChoice === computerChoice) {
-        console.log(`You draw! both chose ${getChoiceFromInt(humanChoice)}`);
-        return "draw";
+        createResultPara(`You draw! both chose ${labelHumanChoice}`, "gold");
     } else if (
         humanChoice - computerChoice === 1 ||
         humanChoice - computerChoice === -2
     ) {
         humanScore++;
-        console.log(
-            `You win the round! ${getChoiceFromInt(humanChoice)} beats ${getChoiceFromInt(computerChoice)}`,
+        createResultPara(
+            `You win the round! ${labelHumanChoice} beats ${labelComputerChoice}`,
+            "green",
         );
-        return "human";
     } else {
         computerScore++;
-        console.log(
-            `You lose the round! ${getChoiceFromInt(computerChoice)} beats ${getChoiceFromInt(humanChoice)}`,
+        createResultPara(
+            `You lose the round! ${labelComputerChoice} beats ${labelHumanChoice}`,
+            "red",
         );
-        return "computer";
     }
+    humanScoreLabel.textContent = humanScore;
+    computerScoreLabel.textContent = computerScore;
 }
 
-function playGame() {
-    let humanChoice;
-    let computerChoice;
-    for (let i = 1; i <= 5; i++) {
-        humanChoice = getHumanChoice();
-        computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-    }
-    displayGameResult();
-}
-
-function displayGameResult() {
-    if (humanScore === computerScore) {
-        console.log(`Draw!`);
-    } else if (humanScore > computerScore) {
-        console.log(`You Win!`);
-    } else {
-        console.log(`You Lose!`);
-    }
-    console.log(`Human Score: ${humanScore}, Computer Score: ${computerScore}`);
+function createResultPara(text, color) {
+    const para = document.createElement("p");
+    para.textContent = text;
+    para.style.color = color;
+    resultDiv.appendChild(para);
 }
