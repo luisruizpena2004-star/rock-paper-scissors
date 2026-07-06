a589400ac2025db4a9095be5603ce56e67a79952
Luis Ruiz Peña <luisruizpena2004@gmail.com>
2026-07-06 13:01:13 -0400

    Fix identation via prettier

--- modules/utils.js
+++ modules/utils.js
@@ -1,14 +1,14 @@
 export function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * max);
 }
 
 export function getChoiceFromInt(val) {
-  switch (val) {
-    case 1:
-      return "Rock";
-    case 2:
-      return "Paper";
-    case 3:
-      return "Scissors";
-  }
+    switch (val) {
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
+    }
 }
